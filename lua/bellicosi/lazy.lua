0cf1cecc6ccd3e238bc14d491bf09f48c808d176
bellicosi-in <73630139+bellicosi-in@users.noreply.github.com>
2024-11-02 22:29:31 +0530

    nvim setup complete

--- lua/bellicosi/lazy.lua
+++ lua/bellicosi/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("bellicosi.plugins", {
+require("lazy").setup({ { import = "bellicosi.plugins" }, { import = "bellicosi.plugins.lsp" } }, {
     checker = {
         enabled = true,
         notify = false,
--- lua/bellicosi/plugins/comment.lua
+++ lua/bellicosi/plugins/comment.lua
@@ -5,15 +5,29 @@ return {
     "JoosepAlviste/nvim-ts-context-commentstring",
   },
   config = function()
-    -- import comment plugin safely
+     -- import comment plugin safely
     local comment = require("Comment")
-
+    
     local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
-
+    
     -- enable comment
     comment.setup({
       -- for commenting tsx, jsx, svelte, html files
       pre_hook = ts_context_commentstring.create_pre_hook(),
+      
+      -- Add keybindings
+      mappings = {
+        -- Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+        basic = true,
+        -- Extra mapping; `gco`, `gcO`, `gcA`
+        extra = true,
+      },
     })
+
+    -- Using leader key for commenting (more reliable across systems)
+    vim.keymap.set('n', '<leader>/', 'gcc', { noremap = false })  -- Comment one line in normal mode
+    vim.keymap.set('v', '<leader>/', 'gc', { noremap = false })   -- Comment multiple lines in visual mode
   end,
 }
+
+
